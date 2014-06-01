bedddee205496f59e4757db6b38876d5931445e2
Chisel <chisel@chizography.net>
2014-06-01 11:31:20 +0100

    Add analytics
    It doesn't appear to work in dev; trying it after a proper install through the
    store

---
+++ js/ga.js
@@ -0,0 +1,20 @@
+// https://developer.chrome.com/extensions/tut_analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-25425412-10']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
+$(document).ready(function() {
+    $('button').click(function(e) {
+        trackButtonClick(e);
+    });
+});
